1a6af1b8d2e774ddb8adec680b918770686d6169
anton <antonmobile2006@gmail.com>
2023-01-05 22:15:19 +0200

    2

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,10 @@ public class Main {
         String on = in.nextLine();
         String chek = "Понедельник";
         String chek2 = "Среда";
-        System.out.println(on);
-        System.out.println(chek.equals(on));
         if(chek.equals(on)) {
             for (int i = 0; i < Tovar.length; i++) {
-                System.out.println(Tovar[i][1]);
                 int number = Integer.parseInt(Tovar[i][1]);
-                System.out.println(number*0.15);
                 double skidka = number-number*0.15;
-                System.out.println(skidka);
                 String skidka2 = String.valueOf(skidka);
                 Tovar[i][1] = skidka2;
                 System.out.println(Tovar[i][0]+" "+Tovar[i][1]);
@@ -23,11 +18,8 @@ public class Main {
             } else {
             if(chek2.equals(on)){
                 for (int i = 0; i < Tovar.length; i++) {
-                    System.out.println(Tovar[i][1]);
                     int number = Integer.parseInt(Tovar[i][1]);
-                    System.out.println(number*0.25);
                     double skidka = number-number*0.25;
-                    System.out.println(skidka);
                     String skidka2 = String.valueOf(skidka);
                     Tovar[i][1] = skidka2;
                     System.out.println(Tovar[i][0]+" "+Tovar[i][1]);
@@ -37,16 +29,5 @@ public class Main {
                     System.out.println(Tovar[i][0]+" "+Tovar[i][1]);
             }
             }
-        int j = 7;
-        do{
-            System.out.println(j);
-            j--;
-        }
-        while (j > 0);
-        int q = 6;
-        while(q<15){
-            System.out.println(q);
-            q++;
-        }
         }
     }
